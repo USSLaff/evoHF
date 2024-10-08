7fad5949104be3ee7e8d3147629c84cd83d0f4a9
Laff <gerreger0170@gmail.com>
2024-10-08 22:09:00 +0200

    class,functions
    still WIP, very likely to change

---
+++ evoHF/evoHF/Decoder.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace evoHF
+{
+	public class Decoder
+	{
+		public char _shortSign = '.';
+		public char _longSign = '-';
+
+		public char ReadSign()
+		{
+			char _read;
+
+
+			if (!char.TryParse(Console.ReadLine(), out _read))
+			{
+				return ReadSign();
+			};
+
+			if (_shortSign == _read)
+			{
+				Console.WriteLine("Short and long signs cannot be the same!");
+				return ReadSign();
+			}
+
+			return _read;
+		}
+
+		public string Decode(string input)
+		{
+
+
+			return "";
+		}
+
+
+
+		public string Encode(string input)
+		{
+
+
+			return "";
+		}
+
+		public override string ToString()
+		{
+			return $"Short sign: {_shortSign}\nLong sign : {_longSign}";
+		}
+	}
+}
---
+++ evoHF/evoHF/Program.cs
@@ -0,0 +1,117 @@
+﻿using System.Text;
+
+namespace evoHF
+{
+	internal class Program
+	{
+		public static Dictionary<char, String> MorseDict = new Dictionary<char, String>()
+			{
+				{'A' , ".-"},
+				{'B' , "-..."},
+				{'C' , "-.-."},
+				{'D' , "-.."},
+				{'E' , "."},
+				{'F' , "..-."},
+				{'G' , "--."},
+				{'H' , "...."},
+				{'I' , ".."},
+				{'J' , ".---"},
+				{'K' , "-.-"},
+				{'L' , ".-.."},
+				{'M' , "--"},
+				{'N' , "-."},
+				{'O' , "---"},
+				{'P' , ".--."},
+				{'Q' , "--.-"},
+				{'R' , ".-."},
+				{'S' , "..."},
+				{'T' , "-"},
+				{'U' , "..-"},
+				{'V' , "...-"},
+				{'W' , ".--"},
+				{'X' , "-..-"},
+				{'Y' , "-.--"},
+				{'Z' , "--.."},
+				{'0' , "-----"},
+				{'1' , ".----"},
+				{'2' , "..---"},
+				{'3' , "...--"},
+				{'4' , "....-"},
+				{'5' , "....."},
+				{'6' , "-...."},
+				{'7' , "--..."},
+				{'8' , "---.."},
+				{'9' , "----."},
+			};
+
+		// '/' for letter spacing, ' ' for word spacing? 
+		// WHAT DO I USE FOR SIGN SPACING????
+
+		static void Main(string[] args)
+		{
+			Decoder _decoder = new Decoder();
+
+			if (ReadConfig())
+			{
+				Console.WriteLine("Reading morse-signs from file...");
+				try
+				{
+					string[] _decodeData = File.ReadAllLines("decodeParams.txt");
+
+					if (_decodeData[0].Equals(_decodeData[1]))
+					{
+						Console.WriteLine("Reading failed!\nShort and long signs cannot be the same!");
+					}
+					else
+					{
+						Console.WriteLine("Reading successful!");
+					}
+				}
+				catch (Exception ex)
+				{
+					Console.WriteLine($"Reading failed:\n{ex.Message}");
+					Console.WriteLine("Resorting to manual reading...");
+				}
+			}
+			else
+			{
+				ReadStart(_decoder);
+			}
+
+			Console.WriteLine(_decoder);
+
+            Console.ReadKey();
+		}
+
+		static void ReadStart(Decoder decoder)
+		{
+			Console.Write("Short sign character: ");
+			decoder._shortSign=decoder.ReadSign();
+			Console.Write("Long sign character: ");
+			decoder._longSign = decoder.ReadSign();
+		}
+
+
+
+		//input scanning with recursion
+		static bool ReadConfig()
+		{
+			char _read;
+			Console.WriteLine("Read morse-signs from config? [y/n]");
+
+			if (!char.TryParse(Console.ReadLine(),out _read)) {
+				return ReadConfig();
+			};
+
+			switch (_read)
+			{
+				case 'n':
+					return false;
+				case 'y':
+					return true;
+				default:
+					return ReadConfig();
+			}
+		}
+	}
+}
